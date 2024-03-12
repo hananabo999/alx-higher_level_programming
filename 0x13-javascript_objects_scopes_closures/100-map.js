7d5e66bf660cf6deaa3970b471e9f3587ea1b3d7
hananabo999 <139076368+hananabo999@users.noreply.github.com>
2024-03-13 01:02:51 +0200

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node 
-
-const { list } = require('./100-data.js');
-const newList = list.map((item, i) => item * i);
-console.log(list);
-console.log(newList);
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-let numOfTimes = 0;
-
-exports.logMe = function (item) {
-	console.log(`${numOfTimes} : ${item}`);
-	numOfTimes++;
-};
